410df052d89cfeea76b243f8514ed5a10203184d
Lepridude <danilaarh07@gmail.com>
2026-07-17 02:39:17 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -7,11 +7,12 @@ CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 def send(text):
     r = requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        json={
+        data={
             "chat_id": CHAT_ID,
             "text": text
         },
         timeout=30
     )
+
     print(r.status_code)
     print(r.text)
