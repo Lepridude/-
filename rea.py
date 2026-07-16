19e20218dd76100fdebdeca0ddf358eded473567
Lepridude <danilaarh07@gmail.com>
2026-07-17 02:07:47 +0300

    Update rea.py

--- rea.py
+++ rea.py
@@ -4,34 +4,24 @@ import requests
 with open("config.json", "r", encoding="utf-8") as f:
     cfg = json.load(f)["rea"]
 
+headers = {
+    "apikey": cfg["jwt"],
+    "Authorization": f"Bearer {cfg['jwt']}"
+}
 
-def get_my_data():
-    headers = {
-        "apikey": cfg["jwt"],
-        "Authorization": f"Bearer {cfg['jwt']}"
-    }
 
-    params = {
-        "select": "*",
-        "competitive_group_id": f"eq.{cfg['competitive_group_id']}",
-        "order": "rating.asc",
-        "offset": 0,
-        "limit": 2000
-    }
-
+def get_all_my_data():
+    # Ищем все направления, где есть твой код
     r = requests.get(
         "https://abitrating.rea.ru/rest/v1/entrants",
         headers=headers,
-        params=params,
+        params={
+            "select": "*",
+            "unique_code_profile": f"eq.{cfg['profile']}",
+            "limit": 100
+        },
         timeout=30
     )
 
     r.raise_for_status()
-
-    data = r.json()
-
-    for row in data:
-        if str(row.get("unique_code_profile")) == cfg["profile"]:
-            return row
-
-    return None
+    return r.json()
