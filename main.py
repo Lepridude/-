17b2f9d037c7086f1a6da5f9cb5b9ac4bc4ef6b9
Lepridude <danilaarh07@gmail.com>
2026-07-17 02:15:41 +0300

    Implement get_group_info function
    Add function to retrieve group information from API

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from rea import get_all_my_data
+def get_group_info(group_id):
+    r = requests.get(
+        "https://abitrating.rea.ru/rest/v1/competitive_groups",
+        headers=headers,
+        params={
+            "select": "*",
+            "id": f"eq.{group_id}"
+        },
+        timeout=30
+    )
 
-print(get_all_my_data())
+    r.raise_for_status()
+
+    data = r.json()
+
+    if data:
+        return data[0]
+
+    return None
