e9faffbd83fb1ab686c05d0b691e3ea0d69aa888
Lepridude <danilaarh07@gmail.com>
2026-07-17 01:55:30 +0300

    Update send function to use json for requests
    Change data parameter to json in send function and log response.

--- telegram.py
+++ telegram.py
@@ -5,11 +5,13 @@ TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 def send(text):
-    requests.post(
+    r = requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={
+        json={
             "chat_id": CHAT_ID,
             "text": text
         },
         timeout=30
     )
+    print(r.status_code)
+    print(r.text)
