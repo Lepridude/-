afe994c80d8140d2774935bbab3becdd61f14e6d
Lepridude <danilaarh07@gmail.com>
2026-07-17 10:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,36 +5,60 @@ from rea import get_all_my_data, get_group_info
 from telegram import send
 
 
+def clean_name(name):
+    name = name.replace("Только", "")
+    name = name.replace("только РФ", "")
+    name = name.replace("РФ", "")
+    name = name.replace("Очная", "")
+    name = name.replace("очная", "")
+    name = name.replace("ВШКМиС", "")
+    name = name.replace("РЭУ им. Г.В. Плеханова", "")
+    name = name.replace("РЭУ", "")
+    name = name.replace("Москва", "")
+
+    name = re.sub(r"(,\s*)+", ", ", name)
+    name = " ".join(name.split()).strip(" ,-()")
+
+    return name
+
+
+text = ""
+
+
+# =========================
+# РЭУ Плеханова
+# =========================
+
+text += "🏛 РЭУ Плеханова\n\n"
+
 rows = get_all_my_data()
-
-text = "🏛 РЭУ Плеханова\n\n"
-
-
-# =========================
-# РЭУ
-# =========================
 
 for row in rows:
     group = get_group_info(row["competitive_group_id"])
+
     print("GROUP FROM REA:", group)
 
     if group:
-        group_name = str(group)
-        group_name = group_name.replace("Только", "")
-        group_name = group_name.replace("только РФ", "")
-        group_name = group_name.replace("РФ", "")
-        group_name = group_name.replace("Очная", "")
-        group_name = group_name.replace("очная", "")
-        group_name = group_name.replace("ВШКМиС", "")
-        group_name = group_name.replace("РЭУ им. Г.В. Плеханова", "")
-        group_name = group_name.replace("РЭУ", "")
-        group_name = group_name.replace("Москва", "")
 
-        group_name = re.sub(r"(,\s*)+", ", ", group_name)
-        group_name = " ".join(group_name.split()).strip(" ,-()")
+        group_name = group.get(
+            "competitive_group_name",
+            group.get("speciality_name", "Неизвестное направление")
+        )
+
+        group_name = clean_name(group_name)
+
+        places = group.get("admission_volume", 0)
 
     else:
         group_name = "Неизвестное направление"
+        places = 0
+
+
+    to_pass = (
+        row["rating"] - places
+        if places
+        else "-"
+    )
 
 
     text += (
@@ -43,6 +67,8 @@ for row in rows:
         f"🎯 Приоритет: {row['priority']}\n"
         f"🏅 ИД: {row['achievements_mark']}\n"
         f"📈 Сумма: {row['sum_mark']}\n"
+        f"🎓 Мест: {places}\n"
+        f"📉 До прохода: {to_pass}\n"
         "━━━━━━━━━━━━━━\n\n"
     )
 
@@ -82,6 +108,7 @@ for group in groups:
         continue
 
     me = misis["my"]
+
 
     text += (
         f"📚 {misis['direction']} — {group['type']}\n"
