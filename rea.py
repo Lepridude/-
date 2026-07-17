ae5d256966d5c9048a00e6072c4f0b01f7f637d9
Lepridude <danilaarh07@gmail.com>
2026-07-17 04:16:30 +0300

    Update rea.py

--- rea.py
+++ rea.py
@@ -1,5 +1,27 @@
 import json
 import requests
+def get_group_info(group_id):
+    r = requests.get(
+        "https://abitrating.rea.ru/rest/v1/competitive_groups",
+        headers=headers,
+        params={
+            "select": "*",
+            "id": f"eq.{group_id}"
+        },
+        timeout=30
+    )
+
+    r.raise_for_status()
+
+    data = r.json()
+
+    print("GROUP ID:", group_id)
+    print("DATA:", data)
+
+    if data:
+        return data[0]
+
+    return None
 
 with open("config.json", "r", encoding="utf-8") as f:
     cfg = json.load(f)["rea"]
